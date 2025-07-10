c711fe52ebc084610732551e39cbb0d5ab238d2d
gafonso <91420802+SelectGabriel@users.noreply.github.com>
2025-07-10 17:02:31 -0300

    loop Principal e primeira funcao

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,53 @@
 #include <iostream>
 using namespace std;
 
+void mensagemEscolhaMenu();
+
 int main() {
     //Menu princiapal do gerenciador de biblioteca
-    int operacaoDesejada = 0;
-    int menuAtual = 0;
-
+    string operacaoDesejada = "";
+    string mensagemLocalAtualLivros = "Você está no gerenciamento de Livros \n";
+    string localAtualEmprestimos = "Você está no gerenciamento de Emprestimos \n";
+    string localAtualUsuarios = "Você está no gerenciamento de usuários \n";
+    int menuPrincipalAtual = 0;
 
     cout << "Olá, seja bem vindo ao sistema de Biblioteca! \n";
     cout << "************************************************ \n";
-    cout << "o que deseja fazer? \n";
-    cout << "************************************************ \n";
 
-    cout << "1. Gerenciar livros \n";
-    cout << "2. Gerenciar emprestimos \n";
-    cout << "3. Gerenciar usuários \n";
+    mensagemEscolhaMenu();
 
     cin >> operacaoDesejada;
 
-    switch(operacaoDesejada) {
-    case 1:
-        break;
-    case 2:
-        break;
-    case 3:
-        cout << "Você está no gerenciamento de usuários \n";
-        menuAtual = 3;
-        break;
-    default:
-        cout << "Desculpe, não entendi seu comando \n";
+    while (operacaoDesejada != "sair")
+    {
+        //Switch case com String não é possível sem uma engenharia adicional, portanto usarei IFs
+        if (operacaoDesejada == "1"){
+            cout << mensagemLocalAtualLivros;
+            menuPrincipalAtual = 1;
+        }
+
+        if (operacaoDesejada == "2"){
+            cout << localAtualEmprestimos;
+            menuPrincipalAtual = 3;
+        }
+
+        if (operacaoDesejada == "3"){
+            cout << localAtualUsuarios;
+            menuPrincipalAtual = 3;
+        }
+
+        mensagemEscolhaMenu();
+        cin >> operacaoDesejada;
     }
+
     return 0;
+}
+
+void mensagemEscolhaMenu(){
+    cout << "************************************************ \n";
+    cout << "o que deseja fazer? \n";
+    cout << "************************************************ \n";
+    cout << "1. Gerenciar livros \n";
+    cout << "2. Gerenciar emprestimos \n";
+    cout << "3. Gerenciar usuários \n";
 }
