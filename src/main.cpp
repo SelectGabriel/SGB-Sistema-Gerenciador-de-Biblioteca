3b3de67b1bc0089a915289a41b9e407bf59add6a
gafonso <91420802+SelectGabriel@users.noreply.github.com>
2025-07-11 13:05:43 -0300

    refactor nos submenus
    me preparando para adicionar ações aos submenus e me livrando de IFs

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,48 @@
 #include <iostream>
+#include <list>
 #include <string>
 using namespace std;
 
+//mensagens
 const string MENSAGEM_LOCAL_ATUAL_LIVROS = "Você está no gerenciamento de Livros \n";
 const string MENSAGEM_LOCAL_ATUAL_EMPRESTIMOS = "Você está no gerenciamento de Emprestimos \n";
 const string MENSAGEM_LOCAL_ATUAL_USUARIOS = "Você está no gerenciamento de usuários \n";
 const string MENSAGEM_ADEUS = "Até logo! \n";
 
+//opções dos menus
+list<string> OPCOES_SUB_MENU_LIVROS = {
+    "1. Adicionar novo livro",
+    "2. Mostrar todos os livros",
+    "3. Buscar livro",
+    "4. Atualizar informações de um livro",
+    "5. Definir um livro como não disponível"
+};
+
+list<string> OPCOES_SUB_MENU_EMPRESTIMOS = {
+    "1. Registrar um empréstimo",
+    "2. Registrar uma devolução",
+    "3. Mostrar todos os livros emprestados de uma pessoa",
+    "4. Mostrar todos os empréstimos ativos",
+};
+
+list<string> OPCOES_SUB_MENU_USUARIOS = {
+    "1. Cadastrar um novo usuário",
+    "2. Mostrar todos os usuários",
+    "3. Buscar um usuário",
+    "4. Inativar um usuário",
+};
+
+//structs
+struct subMenuStruct{
+    int identificador;
+    string mensagem;
+    list<string> opcoes;
+};
+
+//funcoes
 void exibirMensagemMenu();
 int iniciarSubmenu(string subMenu);
-void exibirMensagemSubMenuLivros();
-void exibirMensagemSubMenuEmprestimos();
-void exibirMensagemSubMenuUsuarios();
+subMenuStruct carregarSubMenu(string subMenu);
 
 int main() {
     //Menu princiapal do gerenciador de biblioteca
@@ -34,22 +65,19 @@ int main() {
 
 int iniciarSubmenu(string subMenu){
     string operacaoDesejada;
+    subMenuStruct subMenuCarregado;
 
+    subMenuCarregado = carregarSubMenu(subMenu);
+    cout << subMenuCarregado.mensagem;
     while (operacaoDesejada != "voltar" && operacaoDesejada != "sair")
     {
         cout << "o que deseja fazer?\n";
         cout << "Voce pode:\n";
-        if (subMenu == "1"){
-            exibirMensagemSubMenuLivros();
+        for (string car : subMenuCarregado.opcoes) {
+            cout << car << "\n";
         }
-        if (subMenu == "2"){
-            exibirMensagemSubMenuEmprestimos();
-        }
-        if (subMenu == "3"){
-            exibirMensagemSubMenuUsuarios();
-        }
-        cout << "Voltar. Voltar ao menu anterior \n";
-        cout << "Sair. Sair do sistema \n";
+        cout << "Voltar. ao menu anterior \n";
+        cout << "Sair. do sistema \n";
 
         cin >> operacaoDesejada;
     }
@@ -60,6 +88,30 @@ int iniciarSubmenu(string subMenu){
     return 0;
 }
 
+subMenuStruct carregarSubMenu(string subMenu){
+    subMenuStruct subMenuCarregado; 
+
+    if (subMenu == "1"){
+        subMenuCarregado.identificador = 1;
+        subMenuCarregado.mensagem = MENSAGEM_LOCAL_ATUAL_LIVROS;
+        subMenuCarregado.opcoes = OPCOES_SUB_MENU_LIVROS;
+        return subMenuCarregado;
+    }
+    if (subMenu == "2"){
+        subMenuCarregado.identificador = 2;
+        subMenuCarregado.mensagem = MENSAGEM_LOCAL_ATUAL_EMPRESTIMOS;
+        subMenuCarregado.opcoes = OPCOES_SUB_MENU_EMPRESTIMOS;
+        return subMenuCarregado;
+    }
+    if (subMenu == "3"){
+        subMenuCarregado.identificador = 3;
+        subMenuCarregado.opcoes = OPCOES_SUB_MENU_USUARIOS;
+        return subMenuCarregado;
+    }
+    subMenuCarregado.identificador = 99;
+    return subMenuCarregado;
+}
+
 void exibirMensagemMenu(){
     cout << "************************************************ \n";
     cout << "o que deseja fazer? \n";
@@ -67,27 +119,4 @@ void exibirMensagemMenu(){
     cout << "1. Gerenciar livros \n";
     cout << "2. Gerenciar emprestimos \n";
     cout << "3. Gerenciar usuários \n";
-    cout << "Sair. Sair do sistema \n";
-}
-
-void exibirMensagemSubMenuLivros(){
-    cout << "1. Adicionar novo livro \n";
-    cout << "2. Listar todos os livros \n";
-    cout << "3. Buscar livro \n";
-    cout << "4. Atualizar informações de um livro \n";
-    cout << "5. Definir um livro como não disponível \n";
-}
-
-void exibirMensagemSubMenuEmprestimos(){
-    cout << "1. Registrar um empréstimo \n";
-    cout << "2. Registrar uma devolução \n";
-    cout << "3. Mostrar todos os livros emprestados de uma pessoa \n";
-    cout << "4. Mostrar todos os empréstimos ativos \n";
-}
-
-void exibirMensagemSubMenuUsuarios(){
-    cout << "1. Cadastrar um novo usuário \n";
-    cout << "2. Mostrar todos os usuários \n";
-    cout << "3. Buscar um usuário \n";
-    cout << "4. Remover um usuário \n";
 }
